7a178f3568fcc989b89d1ed914e680b827112e2f
Emanuel Felipe <eemanuelleao@gmail.com>
2023-02-23 00:30:39 -0300

    begin new vite project

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+// libs
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// components
+import { App } from './App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
